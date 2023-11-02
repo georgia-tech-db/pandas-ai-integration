7ee24b3dbed64ddd9767a967a34c7fb7a26d87a8
Ishwarya Sivakumar <ish23.siva@gmail.com>
2023-11-02 01:32:00 -0400

    updated the examples. set to use sqlite. used the new format for querying.

--- chat_runner.py
+++ chat_runner.py
@@ -1,67 +1,66 @@
 import pandas as pd
 import evadb
+import sqlite3
 
 cursor = evadb.connect().cursor()
 print("Connected to EvaDB")
 
+#Drop function if it already exists
+cursor.query("DROP FUNCTION IF EXISTS ChatWithPandas;").execute()
+
+#create a new function
 create_function_query = f"""CREATE FUNCTION IF NOT EXISTS ChatWithPandas
             IMPL  './functions/chat_with_df.py';
             """
-cursor.query("DROP FUNCTION IF EXISTS ChatWithPandas;").execute()
+
 cursor.query(create_function_query).execute()
 print("Created Function")
 
-create_table_query = f"""
-CREATE TABLE IF NOT EXISTS CARSDATA(
-id INTEGER,
-name TEXT(30),
-mpg INTEGER,
-cyl FLOAT(64,64),
-disp FLOAT(64,64),
-hp FLOAT(64,64),
-drat FLOAT(64,64),
-wt FLOAT(64,64),
-qsec FLOAT(64,64),
-vs FLOAT(64,64),
-am FLOAT(64,64),
-gear FLOAT(64,64),
-carb FLOAT(64,64)
-);
+#create table in sqlite and connect to evadb
+sql_db = """CREATE DATABASE IF NOT EXISTS sqlite_data WITH ENGINE = 'sqlite', PARAMETERS = {
+     "database": "evadb.db"
+};"""
+
+cursor.query(sql_db).execute()
+
+#load data into sqlite
+csv_file = 'clean_ml_data/Movie/duplicates/dirty_train.csv'
+df = pd.read_csv(csv_file)
+database_file = 'evadb.db'
+conn = sqlite3.connect(database_file)
+table_name = 'DUPL_DATA'
+df.to_sql(table_name, conn, if_exists='replace', index=False)
+conn.commit()
+conn.close()
+print("Loaded data")
+
+#using the generic cleaning function without any specific instruction
+chat_query_1 = f""" SELECT ChatWithPandas('general cleaning',
+            title, genres, budget, language, duration, year, vote_count, score) FROM sqlite_data.DUPL_DATA;
 """
+result = cursor.query(chat_query_1).execute()
+print(result)
 
+#performing a calculation
+chat_query2 = f""" SELECT ChatWithPandas('what is the mean budget',
+            title, genres, budget, language, duration, year, vote_count, score) FROM sqlite_data.DUPL_DATA;
+"""
+result2 = cursor.query(chat_query2).execute()
+print(result2)
 
-columns = ['id', 'name', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']
+#plotting
+chat_query3 = f""" SELECT ChatWithPandas('plot a chart of year vs budget',
+            title, genres, budget, language, duration, year, vote_count, score) FROM sqlite_data.DUPL_DATA;
+"""
+result3 = cursor.query(chat_query3).execute()
+print(result3)
 
-
-load_data_query = f""" LOAD CSV 'data/cars.csv' INTO CARSDATA;""" 
-
-cursor.query(create_table_query).execute()
-cursor.query(load_data_query).execute()
-
-print("loaded data")
-
-# chat_query1 = f""" SELECT ChatWithPandas('query', 'what is the mean of the gear column',gear, name) FROM CARSDATA;
-# """
-# result1 = cursor.query(chat_query1).execute()
-# print(result1)
-
-# chat_query2 = f""" SELECT ChatWithPandas('plot', 'plot a bar graph of mpg vs hp',mpg, hp) FROM CARSDATA;
-# """
-# result2 = cursor.query(chat_query2).execute()
-# print(result2)
-
-
-# chat_query3 = f""" SELECT ChatWithPandas('manipulation', 'make all the names in the NAME column small', name) FROM CARSDATA;
-# """
-# result3 = cursor.query(chat_query3).execute()
-# print(result3)
-
-chat_query4 = f""" SELECT ChatWithPandas('cleaning',\
-      'impute null values with average of the column if an integer or float. replace with an empty string if column is a string.\
-        remove duplicate rows.', \
-            id, name, mpg, cyl, disp, hp, drat, wt, qsec, vs, am, gear, carb) FROM CARSDATA;
+#data manipulation
+chat_query4 = f""" SELECT ChatWithPandas('make all the names in the title column small', title) 
+              FROM sqlite_data.DUPL_DATA;
 """
 result4 = cursor.query(chat_query4).execute()
 print(result4)
+
 
 
